bf1a367ff70d38ad12713db42705490fbe9e608c
Fcuervo21 <fernando.cuervola@udlap.mx>
2022-10-04 12:09:52 -0500

    Fix API Route

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,16 +1,21 @@
 import { CREATE_POST, FETCH_ALL_POSTS } from "../constants/postsActions";
+import * as api from "../api/index.js";
 
 export const getPosts = () => async (dispatch) =>{
     try {
         // Information about the request
+        const { data } = await api.getPosts();
+        dispatch({ type: FETCH_ALL_POSTS, payload: data });
     } catch (error) {
         console.log(error);
     }
 }
 
-export const newPost = (postInfo) => async (dispatch) =>{
+export const createPost = (newPost) => async (dispatch) =>{
     try {
         // Information about the request
+        const { data } = await api.createPost(newPost);
+        dispatch({ type: CREATE_POST, payload: data });
     } catch (error) {
         console.log(error);
     }
